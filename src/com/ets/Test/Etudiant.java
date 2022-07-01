31cedec77f9bef4aa8e461e703038afcae8b2e77
merlinsohd@yahoo.fr <merlinsohd@yahoo.fr>
2022-06-30 22:27:36 -0400

    Mon premier commit pour le projet Test

---
+++ src/com/ets/Test/Etudiant.java
@@ -0,0 +1,13 @@
+package com.ets.Test;
+
+public class Etudiant {
+
+	public static void main(String[] args) 
+	{
+		
+		  String merlin = "merlin à lui à Dramane";
+		
+		 System.out.println(merlin);
+	}
+
+}
